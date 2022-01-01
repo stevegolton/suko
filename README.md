a4718b4628a46214eaf516550d1e197c46ea89de
Steve Golton <stevegolton@gmail.com>
2022-01-01 16:08:57 +0000

    Load problems from file

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# suko
-
 Suko puzzle solver using constraint programming.
 
 Consider the following (solved) Suko puzzle. The idea is to make the four numbers add up to the total in the middle, and the numbers in the coloured cells add up to the given totals on the right.
@@ -9,3 +7,21 @@ Consider the following (solved) Suko puzzle. The idea is to make the four number
 *Screenshot taken from https://www.transum.org/software/Puzzles/Suko.asp*
 
 This puzzle is an easy candidate for constraint programming. Check out the solution to this example in `suko.py` which uses the [python-constraints](https://pypi.org/project/python-constraint/) library.
+
+## Usage
+
+The puzzle is described using a plaintext config file which looks like this:
+```
+b  g  g
+ 19 24
+b  b  r
+ 18 27
+g  g  r
+
+12 12 21
+```
+
+Run the solver using:
+```
+./suko.py <config_file>
+```
--- suko.py
+++ suko.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from constraint import *
+from parse import parse
+import argparse
 
 # Each cell in the puzzle is assigned a letter from a-i arranged like so:
 #
@@ -10,36 +12,74 @@ from constraint import *
 # --+---+---
 # g | h | i
 
-problem = Problem()
 
-cells = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-possible_values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def load_problem(filename):
+    reds = []
+    blues = []
+    greens = []
 
-# Add each cell as a problem variable
-for cell in cells:
-    problem.addVariable(cell, possible_values)
+    problem = Problem()
 
-# Add a constraint to ensure all variables are unique
-problem.addConstraint(AllDifferentConstraint())
+    cells = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+    possible_values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# Add circle sum constraints
-problem.addConstraint(ExactSumConstraint(19), ('a', 'b', 'd', 'e'))
-problem.addConstraint(ExactSumConstraint(24), ('b', 'c', 'e', 'f'))
-problem.addConstraint(ExactSumConstraint(18), ('d', 'e', 'g', 'h'))
-problem.addConstraint(ExactSumConstraint(27), ('e', 'f', 'h', 'i'))
+    # Add each cell as a problem variable
+    for cell in cells:
+        problem.addVariable(cell, possible_values)
 
-# Add colour sum constraints
-problem.addConstraint(ExactSumConstraint(21), ('b', 'c', 'g', 'h'))
-problem.addConstraint(ExactSumConstraint(12), ('a', 'd', 'e'))
-problem.addConstraint(ExactSumConstraint(12), ('f', 'i'))
+    with open(filename, "r") as file:
+        row0 = parse('{}  {}  {}', file.readline().strip())
+        circles0 = parse('{:d} {:d}', file.readline().strip())
+        row1 = parse('{}  {}  {}', file.readline().strip())
+        circles1 = parse('{:d} {:d}', file.readline().strip())
+        row2 = parse('{}  {}  {}', file.readline().strip())
+        file.readline()
+        colour_sums = parse('{:d} {:d} {:d}', file.readline().strip())
 
-# Solve it, just get the first solution if there are many
-# There should only be one if the problem is constructed correctly
-s = problem.getSolution()
+    colours = [*row0, *row1, *row2]
+    circles = [*circles0, *circles1]
 
-# Print the solution in a friendly manner
-print(' %d | %d | %d' % (s['a'], s['b'], s['c']))
-print('---+---+---')
-print(' %d | %d | %d' % (s['d'], s['e'], s['f']))
-print('---+---+---')
-print(' %d | %d | %d' % (s['g'], s['h'], s['i']))
+    for name, col in zip(cells, colours):
+        if col == "r":
+            reds.append(name)
+        elif col == "b":
+            blues.append(name)
+        elif col == "g":
+            greens.append(name)
+
+    # Add a constraint to ensure all variables are unique
+    problem.addConstraint(AllDifferentConstraint())
+
+    # Add circle sum constraints
+    problem.addConstraint(ExactSumConstraint(circles[0]), ('a', 'b', 'd', 'e'))
+    problem.addConstraint(ExactSumConstraint(circles[1]), ('b', 'c', 'e', 'f'))
+    problem.addConstraint(ExactSumConstraint(circles[2]), ('d', 'e', 'g', 'h'))
+    problem.addConstraint(ExactSumConstraint(circles[3]), ('e', 'f', 'h', 'i'))
+
+    # Add colour sum constraints
+    problem.addConstraint(ExactSumConstraint(colour_sums[0]), reds)
+    problem.addConstraint(ExactSumConstraint(colour_sums[1]), blues)
+    problem.addConstraint(ExactSumConstraint(colour_sums[2]), greens)
+
+    return problem
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str)
+
+    args = parser.parse_args()
+
+    # Load the problem from the passed-in filename
+    problem = load_problem(args.filename)
+
+    # Solve it, just get the first solution if there are many
+    # There should only be one solution, assuming the problem is constructed correctly
+    s = problem.getSolution()
+
+    # Print the solution in a friendly manner
+    print(' %d | %d | %d' % (s['a'], s['b'], s['c']))
+    print('---+---+---')
+    print(' %d | %d | %d' % (s['d'], s['e'], s['f']))
+    print('---+---+---')
+    print(' %d | %d | %d' % (s['g'], s['h'], s['i']))
